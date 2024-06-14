89479a4caca07d3317b0dac0f0937282bcd39682
Azhan Khan <1byteword@gmail.com>
2024-06-14 01:24:55 -0500

    Update barn.rb

--- barn.rb
+++ barn.rb
@@ -1,8 +1,8 @@
 class Barn < Formula
     desc "A simple CLI tool for managing your barn"
     homepage "https://github.com/1byteword/barn"
-    url "https://github.com/1byteword/barn/releases/download/v0.0.1/barn"
-    sha256 "4c617c9a61b0052451cf1de3f1f437403c52aafc95eb026750bfa386c1370d36"
+    url "https://github.com/1byteword/barn/releases/download/v0.0.4/barn"
+    sha256 "810fefc9b62456b278fd34400f1a785d4926ff9967cc7f3fa8fbffbfb5b8dda5"
     license "MIT"
 
     def install
